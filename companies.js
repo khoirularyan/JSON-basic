819843c59e5a2af9db226767a52c2ba1f6cbe915
Khoirularyan <khoirularyan123@gmail.com>
2025-12-23 13:08:52 +0700

    Update company name to BCA Indonesia and increase employees

--- companies.js
+++ companies.js
@@ -1,9 +1,9 @@
 [
   {
-    name: "bbca",
+    name: "BCA Indonesia",
     address: "123 street",
     value: 2000,
-    employees: 50,
+    employees: 100,
     products: ["bank", "loan", "credit card"],
     isActive: true,
   },
