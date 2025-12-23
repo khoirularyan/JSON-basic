09b14c6ac6837ea15b00523548f4275f702fae94
khoirul aryan perdana <khoirularyan123@gmail.com>
2025-12-23 11:47:51 +0700

    Resolve conflicts in companies.js

--- companies.js
+++ companies.js
@@ -20,3 +20,15 @@
     isActive: true,
   },
 ];
+
+
+[
+  {
+    name: "Bank Central Asia",
+    address: "Jakarta Pusat",
+    value: 5000,
+    employees: 50,
+    products: ["bank", "loan", "credit card"],
+    isActive: true,
+  },
+];
