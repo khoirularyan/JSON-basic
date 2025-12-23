cfa02fd2f29d7f7497834eec2b24995a3fab7e91
khoirul aryan perdana <khoirularyan123@gmail.com>
2025-12-23 12:11:05 +0700

    Resolve conflicts in companies.js

--- companies.js
+++ companies.js
@@ -1,29 +1,5 @@
 [
   {
-    name: "BCA Bank",
-    address: "Sudirman Street 123",
-    value: 3000,
-    employees: 50,
-    products: ["bank", "loan", "credit card"],
-    isActive: true,
-  },
-];
-
-
-[
-  {
-    name: "Bank Central Asia",
-    address: "Jakarta Pusat",
-    value: 5000,
-    employees: 50,
-    products: ["bank", "loan", "credit card"],
-    isActive: true,
-  },
-];
-
-
-[
-  {
     name: "Bank Central Asia",
     address: "Jakarta Pusat",
     value: 5000,
