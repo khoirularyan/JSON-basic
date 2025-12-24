678ed4a45922761cf329865d15e2a7466843f47b
Khoirularyan <khoirularyan123@gmail.com>
2025-12-24 14:26:37 +0700

    Fix A: Update products to banking and mortgage

--- companies.js
+++ companies.js
@@ -4,7 +4,7 @@
     address: "Jakarta Pusat",
     value: 5000,
     employees: 80,
-    products: ["bank", "loan", "credit card"],
+    products: ["banking", "mortgage", "credit card"],
     isActive: true,
   },
 ];
