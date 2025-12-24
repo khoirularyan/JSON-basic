9c0c73c4f0a61f07e108d61ffd162db66468e918
Khoirularyan <khoirularyan123@gmail.com>
2025-12-24 13:11:43 +0700

    Team A: Update employees to 80 in both files

--- companies.js
+++ companies.js
@@ -3,7 +3,7 @@
     name: "Bank Central Asia",
     address: "Jakarta Pusat",
     value: 5000,
-    employees: 50,
+    employees: 80,
     products: ["bank", "loan", "credit card"],
     isActive: true,
   },
